149742fa8ac39976315b266dc73ca934a2bfa34f
Volodya <chuchman002@gmail.com>
2023-10-19 12:23:30 +0300

    unit test

---
+++ UnitTest1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab5.0/Lab5.0.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+    TEST_CLASS(UnitTest1)
+    {
+    public:
+
+        TEST_METHOD(TestMethod1)
+        {
+            int a;
+            a = mult(5, 7);
+            Assert::AreEqual(a, 35);
+        }
+    };
+}
